37bc58ac2387026bc8052f48674ac8f2eeddd2e8
Harsh Singh <sharshn21@gmail.com>
2023-04-15 13:26:32 +0530

    Added Comments and make code understandable

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
+# Importing necessary modules
 from movie_recommendation_pipeline import RecommendMovie
 from streamlit_option_menu import option_menu
 import streamlit as st
 import pandas as pd
 import pickle
 
-# Customizing the web app
 st.set_page_config(page_title='Movie Mood', page_icon='😎')
 hide_streamlit_menu = '''
 <style>
@@ -17,56 +17,48 @@ footer {
 </style>
 '''
 
+# Loading the dataframes and lists from pickle files
 movie_info_df = pd.DataFrame.from_dict(pickle.load(open('./pickle files/informative_movies_dict.pkl', 'rb')))
 new_df = pd.DataFrame.from_dict(pickle.load(open('./pickle files/movies_dict.pkl', 'rb')))
 GENRES_LIST = ['Action', 'Adventure', 'Animation', 'Comedy', 'Crime', 'Documentary', 'Drama', 'Family', 'Fantasy', 'Foreign', 'History', 'Horror', 'Music', 'Mystery', 'Romance', 'Science Fiction', 'TV Movie', 'Thriller', 'War', 'Western']
 
+# Creating an instance of the movie recommendation class
 movie_recommender = RecommendMovie()
 
-def get_recommended_movie_columns(names, poster_paths):
-    col1, col2, col3 = st.columns(3)
-    col1.image(poster_paths[0])
-    col1.text(names[0])
-    col2.image(poster_paths[1])
-    col2.text(names[1])
-    col3.image(poster_paths[2])
-    col3.text(names[2])
-    get_free_space()
-    get_free_space()
-    c4, c5, c6 = st.columns(3)
-    c4.image(poster_paths[3])
-    c4.text(names[3])
-    c5.image(poster_paths[4])
-    c5.text(names[4])
-    c6.image(poster_paths[5])
-    c6.text(names[5])
-
+# Defining a function to create some space between the elements
 def get_free_space():
     st.text("")
     st.text("")
     st.text("")
-## App building 
 
+# Building the Streamlit app
 with st.sidebar:
+    # Creating a dropdown menu to select the mode of the app
     selected = option_menu(
         menu_title='Movie Mood 😎',
         options=['Recommend Movies', 'Search Movies']        
     )
 
+# If 'Recommend Movies' mode is selected
 if selected == 'Recommend Movies':
+    # Creating a title and hiding the Streamlit menu
     st.title('Movie Recommender System : ')
     st.markdown(hide_streamlit_menu, unsafe_allow_html=True)
-
     selected_movie_name = st.selectbox('Which movie do you want this system to recommend : ', new_df['title'].values)
     recommend_btn = st.button(label='Recommend')
 
+    # If the 'Recommend' button is clicked and a movie is selected
     if recommend_btn and selected_movie_name:
-        names, poster_paths =  movie_recommender.recommend_movies(movie=selected_movie_name)
+        # Calling the recommend_movies method of the movie recommendation class to get the recommended movies
+        recommended_movies_names, recommended_movies_poster_paths =  movie_recommender.recommend_movies(movie=selected_movie_name)
         get_free_space()
+
+        # Displaying the selected movie and its details
         with st.container():
-            col1, col2 = st.columns([2, 3])        
-            col1.image(movie_recommender.get_poster_path(selected_movie_name), width=250)
+            col1, col2 = st.columns([3, 2])        
+            col1.image(movie_recommender.get_poster_path(selected_movie_name), width=350)
             col2.header(selected_movie_name)
+            
             overview, genres = movie_recommender.get_selected_movie_description(selected_movie_name)
             with st.expander("Description"):
                 col2.write(overview)
@@ -74,30 +66,54 @@ if selected == 'Recommend Movies':
             get_free_space()
             col2.text(genres)
 
+        # Creating some space between the elements
         get_free_space()
         get_free_space()
 
+        # Displaying the recommended movies
         st.subheader('Movies, you would like to watch too ')
         get_free_space()
-            
+
+        # Slicing it to top 20 movies only from the recommended movies
+        recommended_movies_names = recommended_movies_names[:20] if len(recommended_movies_names) > 20 else recommended_movies_names
+        recommended_movies_poster_paths = recommended_movies_poster_paths[:20] if len(recommended_movies_poster_paths) > 20 else recommended_movies_poster_paths
+
         with st.container():
-            get_recommended_movie_columns(names=names, poster_paths=poster_paths)
+            if len(recommended_movies_names):
+                for index, name in enumerate(recommended_movies_names):
+                    film_description, film_genres = movie_recommender.get_selected_movie_description(movie_name=name)
+                    col1, col2 = st.columns(2)
+                    col1.image(recommended_movies_poster_paths[index], width=290)
+                    col2.header(name)
+                    get_free_space()
+                    with st.expander('Overview'):
+                        col2.write(film_description)
+                    get_free_space()
+                    get_free_space()
+                    col2.text(film_genres)
+
+    # If the 'Recommend' button is clicked but no movie is selected
     elif recommend_btn and  selected_movie_name == []:
         st.markdown('Please enter something')
 
+# If 'Search Movies' mode is selected
 else:
+    # Creating a title and hiding the Streamlit menu
     st.title('Search Movies through their Genres')
     st.markdown(hide_streamlit_menu, unsafe_allow_html=True)
+
     get_free_space()
     st.subheader('Not getting the Movie Name but know it\'s genres,')
     st.subheader('Search here : ')
     get_free_space()
-    choices = st.multiselect('Enter Genres', options=GENRES_LIST)
+    choices = st.multiselect('', options=GENRES_LIST)
     search_movies = st.button(label='Search Movies')
     if search_movies and choices:
+        # Defining recommended movies based on genres based on the choices given by the user
         genres_recommended_movies = movie_recommender.get_movies_based_on_genres(genres_list=choices)
         genres_recommended_movies = genres_recommended_movies[:15] if len(genres_recommended_movies) > 15 else genres_recommended_movies
         genres_recommended_movies_poster_paths = []
+        # Running a for loop to get the poster_paths of each movie in genres_recommended_movies
         for genre_movie in genres_recommended_movies:
             if movie_recommender.get_poster_path(genre_movie) != None:
                 genres_recommended_movies_poster_paths.append(movie_recommender.get_poster_path(genre_movie))
@@ -107,6 +123,7 @@ else:
         get_free_space()
         get_free_space()
 
+        # Making the columns to display the recommended movies
         if len(genres_recommended_movies):
             for index, movie in enumerate(genres_recommended_movies):
                 c1, c2 = st.columns(2)
--- movie_recommendation_pipeline.py
+++ movie_recommendation_pipeline.py
@@ -1,3 +1,4 @@
+# Import necessary libraries
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import streamlit as st
@@ -6,44 +7,53 @@ import pandas as pd
 import requests
 import os
 
+# Load data from pickle files
 movie_info_df = pd.DataFrame.from_dict(pickle.load(open('./pickle files/informative_movies_dict.pkl', 'rb')))
 new_df = pd.DataFrame.from_dict(pickle.load(open('./pickle files/movies_dict.pkl', 'rb')))
 
+# Define constants
 # API_KEY = st.secrets['secrets']["auth_token"]
-API_KEY = os.getenv('auth_token')
+API_KEY = os.getenv('auth_token')  # Load API key from environment variable
 GENRES_LIST = ['Action', 'Adventure', 'Animation', 'Comedy', 'Crime', 'Documentary', 'Drama', 'Family', 'Fantasy', 'Foreign', 'History', 'Horror', 'Music', 'Mystery', 'Romance', 'Science Fiction', 'TV Movie', 'Thriller', 'War', 'Western']
 
-
-
+# Initialize count vectorizer and calculate cosine similarity matrix
 cv = CountVectorizer(max_features=5000, stop_words='english')
 vectors = cv.fit_transform(new_df['tags']).toarray()
 similarity = cosine_similarity(vectors)
 
+# Create class to recommend movies
 class RecommendMovie():
     def __init__(self) -> None:
+        # Load data into class variables
         global movie_info_df, new_df
         self.df = new_df
         self.info_df = movie_info_df
     
     def recommend_movies(self, movie):
-            global similarity
-            movie_index = self.df[self.df['title'] == movie].index[0]
-            movie_similarity = similarity[movie_index]
-            self.movies = sorted(list(enumerate(movie_similarity)), reverse=True, key=lambda x:x[1])[1:7]
-            recommended_movies = []
-            posters = []
-            for movie in self.movies:
-                movie_id = self.df['movie_id'][movie[0]]
-                response = requests.get(f'https://api.themoviedb.org/3/movie/{movie_id}?api_key={API_KEY}&language=en-US')
-                data = response.json()
-                if data['poster_path'] is not None:
-                    posters.append('http://image.tmdb.org/t/p/w500' + data['poster_path'])
-                else:
-                    posters.append('https://previews.123rf.com/images/urfandadashov/urfandadashov1806/urfandadashov180601827/150417827-photo-not-available-vector-icon-isolated-on-transparent-background-photo-not-available-logo-concept.jpg')
-                recommended_movies.append(self.df['title'][movie[0]])
-            return recommended_movies, posters
+        '''Returns recommended movies based on the movie name given '''
+        # Find movie index and calculate similarity scores
+        global similarity
+        movie_index = self.df[self.df['title'] == movie].index[0]
+        movie_similarity = similarity[movie_index]
+        # Sort movies by similarity score and select top 6
+        self.movies = sorted(list(enumerate(movie_similarity)), reverse=True, key=lambda x:x[1])[1:7]
+        recommended_movies = []
+        posters = []
+        # Retrieve poster path for recommended movies
+        for movie in self.movies:
+            movie_id = self.df['movie_id'][movie[0]]
+            response = requests.get(f'https://api.themoviedb.org/3/movie/{movie_id}?api_key={API_KEY}&language=en-US')
+            data = response.json()
+            if data['poster_path'] is not None:
+                posters.append('http://image.tmdb.org/t/p/w500' + data['poster_path'])
+            else:
+                posters.append('https://previews.123rf.com/images/urfandadashov/urfandadashov1806/urfandadashov180601827/150417827-photo-not-available-vector-icon-isolated-on-transparent-background-photo-not-available-logo-concept.jpg')
+            recommended_movies.append(self.df['title'][movie[0]])
+        return recommended_movies, posters
     
     def get_poster_path(self, film):
+        '''Returns the movie's poster path based on the tmdb api'''
+        # Find movie index and retrieve poster path
         film_index = self.df[self.df['title'] == film].index[0]
         film_id = self.df['movie_id'][film_index]
         response = requests.get(f'https://api.themoviedb.org/3/movie/{film_id}?api_key={API_KEY}&language=en-US')
@@ -51,8 +61,10 @@ class RecommendMovie():
         if data['poster_path'] is not None:
             return 'http://image.tmdb.org/t/p/w500' + data['poster_path']
         return 'https://previews.123rf.com/images/urfandadashov/urfandadashov1806/urfandadashov180601827/150417827-photo-not-available-vector-icon-isolated-on-transparent-background-photo-not-available-logo-concept.jpg'
-    
+
+
     def get_selected_movie_description(self, movie_name):
+        '''returns the selected movie's description'''
         movie_index = self.df[self.df['title'] == movie_name].index[0]
         movie_overview = self.info_df['overview'][movie_index]
         genres = self.info_df['genres'][movie_index]
@@ -63,9 +75,11 @@ class RecommendMovie():
         return ' '.join(movie_overview), ', '.join(genres) 
     
     def get_movies_based_on_genres(self, genres_list):
+        '''returns list of movies that have the same genres given in the parameter genres_list'''
         genres_recommended_movies = []
         for movie in self.info_df['title']:
             movie_index = self.info_df[self.info_df['title'] == movie].index[0]
+            # Finding Movie genres
             movie_genres = self.info_df['genres'][movie_index]
             total_math_percentage = 0.0
             for movie_genres in movie_genres:
@@ -74,6 +88,7 @@ class RecommendMovie():
                     if selected_genre in movie_genres:
                         match_count += 1
                 total_math_percentage +=  match_count / len(genres_list)
+            # Seeing if the total match is more than 60%
             if total_math_percentage >= 0.8:
                 genres_recommended_movies.append(movie)
         return genres_recommended_movies
